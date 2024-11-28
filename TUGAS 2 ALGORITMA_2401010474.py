0e71ab44f93b09ae5b19c95a0981808016cfc921
NiPutuAmberleaOkaRini <putuamberleaokarini@gmail.com>
2024-11-28 18:27:25 +0700

    Upload Tugas 2 Algoritma

---
+++ TUGAS 2 ALGORITMA_2401010474.py
@@ -0,0 +1,24 @@
+"""
+Tugas 2 Algoritma dan Logika Informatika
+Nama : Ni Putu Amberlea Oka Rini
+NIM : 2401010474
+Kelas : K
+Prodi : IF-KAB
+Konversi Satuan Dari Meter(m) ke Centimeter(cm)
+"""
+
+#Langkah 1 : Input nilai dalam meter
+meter = int(input("Masukkan nilai dalam meter: ")) 
+
+#Langkah 2: Konversi meter ke centimeter
+#1 meter = 100 centimeter
+centimeter = meter * 100 
+
+#Langkah 3: Proses
+txhasil=(f"{meter} m = {centimeter} cm")
+
+#Mencetak hasil (Output)
+print(txhasil)
+
+
+
